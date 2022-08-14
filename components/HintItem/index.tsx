a597be5cc57c4ac55dd8ed2d1303faa981b7fc89
Arian Mirahmadi <mirarianmir@gmail.com>
2022-08-14 06:46:34 +0430

    fix: SideNav badge colors and HintItem transition

--- components/HintItem/index.tsx
+++ components/HintItem/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 const HintItem: NextPage<Props> = ({ children, activated }) => {
   return (
     <div className={
-        `flex items-center px-2 py-0.5 hover:bg-gray-100 hover:text-gray-700 font-semibold rounded ${activated ? 'text-sky-600 font-bold' : ''}`
+        `flex items-center px-2 py-0.5 transition-color hover:bg-gray-100 hover:text-gray-700 font-semibold rounded ${activated ? 'text-sky-600 font-bold' : ''}`
     }>
       { children }
     </div>
